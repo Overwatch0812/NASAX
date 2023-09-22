b1641357ea98aa0f650bc6d72c0ab4d385dbf199
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-09-22 13:15:13 +0530

    router bug fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,54 +1,54 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./components/Layout";
+
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Activate from "./components/Activate";
 import Homes from "./components/Homes";
 import ResetPassword from "./components/ResetPassword";
 import ResetPasswordConfirm from "./components/ResetPasswordConfirm";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-    },
-    {
-      path: "/signup",
-      element: <Signup />,
-    },
-    {
-      path: "/home",
-      element: <Homes />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/activate/:uid/:token",
-      element: <Activate />,
-    },
-    {
-      path: "/reset-password",
-      element: <ResetPassword />,
-    },
-    {
-      path: "/password/reset/confirm/:uid/:token",
-      element: <ResetPasswordConfirm />,
-    },
-  ]);
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Homes />,
+		},
+		{
+			path: "/signup",
+			element: <Signup />,
+		},
+		// {
+		// 	path: "/home",
+		// 	element: <Homes />,
+		// },
+		{
+			path: "/login",
+			element: <Login />,
+		},
+		{
+			path: "/activate/:uid/:token",
+			element: <Activate />,
+		},
+		{
+			path: "/reset-password",
+			element: <ResetPassword />,
+		},
+		{
+			path: "/password/reset/confirm/:uid/:token",
+			element: <ResetPasswordConfirm />,
+		},
+	]);
 
-  return (
-    <>
-      <RouterProvider router={router}>
-        <Layout >
-          <Homes/>
-        </Layout>
-      </RouterProvider>
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<RouterProvider router={router} />
+			<Footer />
+		</>
+	);
 }
 
 export default App;
