0200c6c8f81a7b550a89db6ec50f175a979aebb0
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-10-01 11:15:48 +0530

    Merge Changes

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,10 +1,10 @@
 import { LuFolderSearch } from "react-icons/lu";
-
+import Select from "react-select";
 export default function Search() {
 	return (
 		<div className="max-w-[1200px] w-full flex justify-between md:justify-evenly md:gap-[10rem] items-center my-3">
 			<div className="">
-				<h3 className=" my-2 text-2xl font-semibold">Home</h3>
+				<h3 className=" my-2 text-2xl font-semibold">Feed</h3>
 			</div>
 			<div className="bg-cardGrey flex gap-3 px-2 rounded-md md:w-1/3 md:justify-between">
 				<input
