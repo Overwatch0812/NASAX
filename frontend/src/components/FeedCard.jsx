32ed78406345b88d4b31ba41d3c7d0ccc9d5104e
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-10-02 23:10:31 +0530

    Pull Master

--- frontend/src/components/FeedCard.jsx
+++ frontend/src/components/FeedCard.jsx
@@ -2,24 +2,19 @@ import { useEffect } from "react";
 import axios from "axios";
 
 export default function FeedCard(props) {
-  return (
-
-     
-        <div className=" flex flex-col h-[350px] overflow-hidden ">
-          <img src={props.thumbnail} alt="" className="rounded-t-md"/>
-          <div className="flex flex-col gap-3">
-            <div>
-              
-              <p>{props.author}</p>
-            </div>
-            <div>
-              <p>{props.domain}</p>
-            </div>
-          </div>
-          <h3>{props.title}</h3>
-          <h4>{props.description}</h4>
-        </div>
-
-  
-  );
+	return (
+		<div className=" flex flex-col h-[350px] overflow-scroll no-scroll">
+			<img src={props.thumbnail} alt="" className="rounded-t-md" />
+			<div className="flex flex-col gap-3">
+				<div>
+					<p>{props.author}</p>
+				</div>
+				<div>
+					<p>{props.domain}</p>
+				</div>
+			</div>
+			<h3>{props.title}</h3>
+			<h4>{props.description}</h4>
+		</div>
+	);
 }
