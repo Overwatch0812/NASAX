c854c3dff6fda000876e75814f923106e762096c
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-10-04 18:54:56 +0530

    Carousel Done

--- frontend/src/components/FeedPage.jsx
+++ frontend/src/components/FeedPage.jsx
@@ -7,13 +7,33 @@ import { reset } from "../features/projects/projectSlice";
 import { fetchProjectApiData } from "../features/projects/projectSlice";
 import { Link } from "react-router-dom";
 import Spinner from "./Spinner";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 export default function FeedPage() {
 	const dispatch = useDispatch();
 	const [cardData, setCardData] = useState([]);
 
 	const { user, isSuccess } = useSelector((state) => state.auth);
-
+	const responsive = {
+		superLargeDesktop: {
+			// the naming can be any, depends on you.
+			breakpoint: { max: 4000, min: 3000 },
+			items: 5,
+		},
+		desktop: {
+			breakpoint: { max: 3000, min: 1024 },
+			items: 5,
+		},
+		tablet: {
+			breakpoint: { max: 1024, min: 464 },
+			items: 2,
+		},
+		mobile: {
+			breakpoint: { max: 464, min: 0 },
+			items: 1,
+		},
+	};
 	//
 
 	// useEffect(() => {
@@ -38,9 +58,19 @@ export default function FeedPage() {
 	return !cardData ? (
 		<h1 className="text-[#00df9a]">Data is Being Fetched</h1>
 	) : (
-		<div className="px-4 mx-3 lg:mx-auto flex flex-col text-white">
-			<Search />
-			<div
+		<div className="px-8 mx-3 lg:mx-auto flex flex-col text-white">
+			{/* <Search /> */}
+			<Carousel responsive={responsive}>
+				{cardData.map((project) => {
+					return (
+						<Link to={"/project/" + project.id} key={project.id}>
+							<FeedCard {...project} />
+						</Link>
+					);
+				})}
+			</Carousel>
+
+			{/* <div
 				className="my-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 md:gap-6 lg:gap-7 xl:gap-8
 			"
 			>
@@ -51,7 +81,7 @@ export default function FeedPage() {
 						</Link>
 					);
 				})}
-			</div>
+			</div> */}
 		</div>
 	);
 }
