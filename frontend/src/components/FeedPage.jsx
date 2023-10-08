8b5cab26691af4c04288eb7aaa725ad8f5699633
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-10-08 13:08:18 +0530

    Search feature done

--- frontend/src/components/FeedPage.jsx
+++ frontend/src/components/FeedPage.jsx
@@ -12,50 +12,50 @@ import "react-multi-carousel/lib/styles.css";
 import Shimmer from "./Shimmer";
 
 export default function FeedPage() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const [cardData, setCardData] = useState([]);
-  const [revCardData, setRevCardData] = useState([]);
-  // const { user, isSuccess } = useSelector((state) => state.auth);
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 4,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 1,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
-  //
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const [cardData, setCardData] = useState([]);
+	const [revCardData, setRevCardData] = useState([]);
+	// const { user, isSuccess } = useSelector((state) => state.auth);
+	const responsive = {
+		superLargeDesktop: {
+			// the naming can be any, depends on you.
+			breakpoint: { max: 4000, min: 3000 },
+			items: 5,
+		},
+		desktop: {
+			breakpoint: { max: 3000, min: 1024 },
+			items: 4,
+		},
+		tablet: {
+			breakpoint: { max: 1024, min: 464 },
+			items: 1,
+		},
+		mobile: {
+			breakpoint: { max: 464, min: 0 },
+			items: 1,
+		},
+	};
+	//
 
-  const { user, isSuccess, isUserLoaded, IsError } = useSelector(
-    (state) => state.auth
-  );
+	const { user, isSuccess, isUserLoaded, IsError } = useSelector(
+		(state) => state.auth
+	);
 
-  //
+	//
 
-  useEffect(() => {
-    if (isUserLoaded) {
-      dispatch(fetchProjectApiData()).then((e) => setCardData(e.payload));
-    }
-  }, [isUserLoaded, user, IsError, isSuccess, dispatch, navigate]);
-
+	useEffect(() => {
+		if (isUserLoaded) {
+			dispatch(fetchProjectApiData()).then((e) => setCardData(e.payload));
+		}
+	}, [isUserLoaded, user, IsError, isSuccess, dispatch, navigate]);
 
 	async function getCardData() {
 		const res = await fetch(
 			"https://manage-leszjnj0o-overwatch0812.vercel.app/api/"
 		);
 		const data = await res.json();
+		console.log(data);
 		setCardData(data);
 		setRevCardData(data);
 	}
@@ -63,51 +63,60 @@ export default function FeedPage() {
 		getCardData();
 	}, []);
 
-
-  return cardData.length === 0 ? (
-    <div className="px-8 mx-3 lg:mx-auto my-4 text-white w-full flex flex-col gap-8">
-      <Shimmer />
-    </div>
-  ) : (
-    <div className="px-8 mx-3 lg:mx-auto my-4 text-white w-full flex flex-col gap-8">
-      {/* <Search /> */}
-      <div>
-        <div className="flex w-full justify-center lg:justify-start my-3">
-          <h1 className="text-2xl font-semibold">Trending Projects</h1>
-        </div>
-        <Carousel responsive={responsive} itemClass="pr-6">
-          {cardData.map((project) => {
-            return (
-              <Link to={"/project/" + project.id} key={project.id}>
-                <FeedCard {...project} />
-              </Link>
-            );
-          })}
-        </Carousel>
-      </div>
-      <div>
-        {revCardData.length === 0 ? (
-          <div className="flex w-full justify-center lg:justify-start my-3">
-            <Shimmer />
-          </div>
-        ) : (
-          <>
-            <div className="flex w-full justify-center lg:justify-start my-3">
-              <h1 className="text-2xl font-semibold">Recommended For You </h1>
-            </div>
-            <Carousel responsive={responsive} itemClass="pr-6">
-              {revCardData.map((project) => {
-                return (
-                  <Link to={"/project/" + project.id} key={project.id}>
-                    <FeedCard {...project} />
-                  </Link>
-                );
-              })}
-            </Carousel>
-          </>
-        )}
-      </div>
-      {/* <div
+	return cardData.length === 0 ? (
+		<div className="px-8 mx-3 lg:mx-auto my-4 text-white w-full flex flex-col gap-8">
+			<Shimmer />
+		</div>
+	) : (
+		<div className="px-8 mx-3 lg:mx-auto my-4 text-white w-full flex flex-col gap-8">
+			{/* <Search /> */}
+			<div>
+				<div className="flex w-full justify-center lg:justify-start my-3">
+					<h1 className="text-2xl font-semibold">
+						Trending Projects
+					</h1>
+				</div>
+				<Carousel responsive={responsive} itemClass="pr-6">
+					{cardData.map((project) => {
+						return (
+							<Link
+								to={"/project/" + project.id}
+								key={project.id}
+							>
+								<FeedCard {...project} />
+							</Link>
+						);
+					})}
+				</Carousel>
+			</div>
+			<div>
+				{revCardData.length === 0 ? (
+					<div className="flex w-full justify-center lg:justify-start my-3">
+						<Shimmer />
+					</div>
+				) : (
+					<>
+						<div className="flex w-full justify-center lg:justify-start my-3">
+							<h1 className="text-2xl font-semibold">
+								Recommended For You{" "}
+							</h1>
+						</div>
+						<Carousel responsive={responsive} itemClass="pr-6">
+							{revCardData.map((project) => {
+								return (
+									<Link
+										to={"/project/" + project.id}
+										key={project.id}
+									>
+										<FeedCard {...project} />
+									</Link>
+								);
+							})}
+						</Carousel>
+					</>
+				)}
+			</div>
+			{/* <div
 				className="my-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 md:gap-6 lg:gap-7 xl:gap-8
 			"
 			>
@@ -119,6 +128,6 @@ export default function FeedPage() {
 					);
 				})}
 			</div> */}
-    </div>
-  );
+		</div>
+	);
 }
